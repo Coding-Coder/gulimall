8d0fb94240a151f25afb74a488b36a2a76771854
lxy <aethon47@163.com>
2021-05-29 10:50:46 +0800

    feat:谷粒商城项目初始化

---
+++ gulimall-coupon/src/test/java/com/lxy/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxy.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
